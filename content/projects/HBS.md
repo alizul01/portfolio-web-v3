7fed41232b066cb0206cc6a145d2e291599b271d
Multimedia <alizulfikar032@gmail.com>
2023-12-01 18:54:14 +0700

    init project

---
+++ content/projects/HBS.md
@@ -0,0 +1,11 @@
+---
+date: '2022-12-01'
+title: 'Pekan Ilmiah Mahasiswa Nasional 36'
+github: ''
+external: 'https://jti.polinema.ac.id/index.php/2022/10/21/2-tim-mahasiswa-jurusan-teknologi-informasi-berhasil-lolos-pimnas-2022/'
+tech:
+  - Figma
+  - Canon EOS M50
+company: 'Puspresnas'
+showInProjects: true
+---
---
+++ src/components/sections/about.js
@@ -0,0 +1,186 @@
+import React, { useEffect, useRef } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    grid-gap: 0 10px;
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
+
+    &:hover,
+    &:focus {
+      outline: 0;
+      transform: translate(-4px, -4px);
+
+      &:after {
+        transform: translate(8px, 8px);
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 14px;
+      left: 14px;
+      z-index: -1;
+    }
+  }
+`;
+
+const About = () => {
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  const skills = ['Unity', 'Next JS', 'C-Sharp', 'Typescript', 'Blender 3D', 'Tailwind CSS'];
+
+  return (
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">About Me</h2>
+
+      <div className="inner">
+        <StyledText>
+          <div>
+            <p>
+              Hello! My name is Ali Zulfikar, and I have a passion for game and website development. I possess skills in leadership, teamwork, and public speaking. I'm a creative, innovative, and adaptive individual. Recently
+            </p>
+
+            <p>
+              I have several of achievement in the field of Game Development and Innovation. Such as {' '}
+              <a href="https://gemastik.kemdikbud.go.id/">4th Winner Gemastik 2023</a>,{' '}
+              <a href="https://kmipn.pens.ac.id/">Special Reward KMIPN 2023 </a>,{' '}
+              <a href="https://kompetisi4c.ub.ac.id/">3rd Winner 4C Competition</a>, and{' '}
+              <a href={'https://igdx.id'}>Indonesia Game Developer Exchange 2023 (IGDX 2023)</a>. My
+              main focus right now is develop my skills in Game Development and Web Development.
+            </p>
+
+            <p>
+              I also recently being Mentee at{' '}
+              <a href="https://agate.id/">
+                Agate Course 2023
+              </a>{' '}
+              that educate me about AI Game Development using Unity. I Also building my own Game Studio called{' '}
+              <a href="https://www.instagram.com/enigmates.studio/" target="_blank" rel="noreferrer">
+                Enigmates Studio
+              </a>{' '}
+            </p>
+
+            <p> Anyway, this is technology that I usually use for my project: </p>
+          </div>
+
+          <ul className="skills-list">
+            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+        </StyledText>
+
+        <StyledPic>
+          <div className="wrapper">
+            <StaticImage
+              className="img"
+              src="../../images/me.jpg"
+              width={500}
+              quality={95}
+              formats={['AUTO', 'WEBP', 'AVIF']}
+              alt="Headshot"
+            />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
